80e2a2a5415f72fb5cc44527cd0ee61bc90dc03e
wpinedaCrack <wapcamargo@gmail.com>
2024-05-08 22:55:39 -0500

    first commit

---
+++ Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
@@ -0,0 +1,12 @@
+﻿using Tarker.Booking.Domain.Entities.Booking;
+
+namespace Tarker.Booking.Domain.Entities.Customer
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string DocumentNumber { get; set; }
+        public ICollection<BookingEntity> Bookings { get; set; }
+    }
+}
---
+++ Tarker.Booking.Domain/Entities/User/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using Tarker.Booking.Domain.Entities.Booking;
+
+namespace Tarker.Booking.Domain.Entities.User
+{
+    public class UserEntity
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public ICollection<BookingEntity> Bookings { get; set; }
+    }
+}
